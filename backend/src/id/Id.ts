302b097c03f618fce8ec620c35a5318a217105e7
Wojciech Nadurski <w.nadurski@gmail.com>
2021-03-23 12:13:39 +0100

    used fp-ts library for newtype

--- backend/src/id/Id.ts
+++ backend/src/id/Id.ts
@@ -1,6 +1,7 @@
-import { Newtype, wrap } from "../utils/Newtype"
 import { v4 } from "uuid"
+import { iso, Newtype } from "newtype-ts"
 
-export interface Id extends Newtype<string> {}
+export interface Id extends Newtype<{ readonly Id: unique symbol }, string> {}
+const isoId = iso<Id>()
 
-export const createId = (): Id => wrap(v4())
+export const createId = (): Id => isoId.wrap(v4())
