ca14e1ec51899b9fc4e93911deb09af4975b5952
gnadurski <twoeleven@Protonmail.com>
2021-04-02 19:36:54 +0200

    login

--- frontend/src/auth/services.ts
+++ frontend/src/auth/services.ts
@@ -1,27 +1,41 @@
-export async function getCurrentUser(): Promise<{
-  user: string
-  token: string
-}> {
-  // eslint-disable-next-line no-undef
-  return fetch(process.env.PUBLIC_URL + "/usertoken.json").then((res) =>
-    res.json()
-  )
-}
+import { User } from "../User"
 
-export async function loginAttempt(
-  username: string,
-  password: string,
-  requestOps = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ user: username, pass: password }),
+export async function getCurrentUser(): Promise<User | undefined> {
+  const token = window.localStorage.getItem("token")
+
+  const requestOps = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token || "invalid"}`,
+    },
   }
-): Promise<{
-  user: string
-  token: string
-}> {
+  // eslint-disable-next-line no-undef
   return fetch(
     process.env.PUBLIC_URL + "/usertoken.json",
     requestOps
   ).then((res) => res.json())
+}
+
+export async function loginAttempt(
+  username: string,
+  password: string
+): Promise<
+  | {
+      token: string
+    }
+  | undefined
+> {
+  const requestOps = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  }
+  return fetch("/api/auth/login", requestOps).then((res) => {
+    if (res.status !== 200) {
+      return undefined
+    }
+
+    return res.json()
+  })
 }
