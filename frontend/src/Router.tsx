7633e0e071bdbd16bcfaaca63ec14f0eb8b94276
gnadurski <twoeleven@protonmail.com>
2021-04-15 16:53:46 +0200

    added HashRouter inside Router

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -2,8 +2,17 @@ import WelcomePage from "./pages/WelcomePage"
 import LoginForm from "./auth/LoginForm"
 import React, { ReactElement, useContext } from "react"
 import { AuthContext } from "./auth/AuthProvider"
+import { HashRouter } from "react-router-dom"
 
 export const Router = (): ReactElement => {
   const { user } = useContext(AuthContext)
-  return user ? <WelcomePage /> : <LoginForm />
+  return user ? (
+    <HashRouter>
+      <WelcomePage />{" "}
+    </HashRouter>
+  ) : (
+    <HashRouter>
+      <LoginForm />
+    </HashRouter>
+  )
 }
