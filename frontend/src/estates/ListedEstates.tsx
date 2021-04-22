aa92da44088cd053d0d8db4141eafb9bc54ca2b1
gnadurski <twoeleven@protonmail.com>
2021-04-22 17:29:53 +0200

    estates list working locally on initialized array

--- frontend/src/estates/ListedEstates.tsx
+++ frontend/src/estates/ListedEstates.tsx
@@ -1,19 +1,50 @@
 import React, { ReactElement } from "react"
 import { Link } from "react-router-dom"
 import { Card } from "antd"
+import EstateAdder from "./EstateAdder"
+
+interface Estate {
+  id: string
+  name: string
+  address: string
+  cost: string
+  tenant: string
+}
 
 const ListedEstates = (): ReactElement => {
+  const estatesList: Estate[] = [
+    {
+      id: "1",
+      name: "garaz",
+      address: "Sloneczna 11",
+      cost: "1000",
+      tenant: "nikt",
+    },
+    {
+      id: "2",
+      name: "kwiaciarnia",
+      address: "Ksiezycowa 2",
+      cost: "500",
+      tenant: "ktos",
+    },
+  ]
+
   return (
     <div>
       <div>Your estates:</div>
-      <Card
-        title="Name of estate"
-        extra={<Link to="/estates/StarterEstate">Details</Link>}
-      >
-        <p>Address: </p>
-        <p>Cost: </p>
-        <p>Tenant: </p>
-      </Card>
+
+      {estatesList.map((estate) => {
+        return (
+          <Card
+            title={estate.name}
+            extra={<Link to={`/estates/${estate.id}`}>Details</Link>}
+          >
+            <p>Address: {estate.address}</p>
+            <p>Cost: {estate.cost}</p>
+            <p>Tenant: {estate.tenant}</p>
+          </Card>
+        )
+      })}
       <Link to="/estates/add">Add estates</Link>
       <Link to="/">Home</Link>
     </div>
